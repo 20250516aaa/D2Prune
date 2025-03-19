48c94a3b707b981785d9421a6849086010a9518c
xionglang <840015412@qq.com>
2025-03-19 15:11:56 +0800

    update sh

--- scripts/opt-125m.sh
+++ scripts/opt-125m.sh
@@ -1,15 +1,13 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --model ../cache/llm_weights/models--facebook--opt-125m/snapshots/opt-125m \
-    --sparsity_ratio 0. \
+    --sparsity_ratio 0.5 \
     --nsamples 128 \
-    --sparsity_type unstructured \
-    --prune_method d2prune \
     --device cuda:0 \
     --cali_dataset c4 \
     --cali_data_path ../cache/data/c4 \
     --eval_dataset wikitext2 \
     --eval_data_path ../cache/data/wikitext \
-    --output_dir out/opt-125m-dense/ \
+    --output_dir out/opt-125m-d2prune-sp0.5/ \
     --s 1500 \
     --r1 1 \
     --r2 0 \
